cde22fd185741a76166079a183a10257b04fa299
AntoMonti <a.matviishyn@hotmail.com>
2019-12-17 22:43:18 +0200

    shift down some code

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     text { "Article text" }
 
      # создаём фабрику для создания статьи с несколькими комментариями
-    factory :article_with_comments do
-      # после создания article
-    after :create do |article, evaluator|
-        # создаём список из 3-х комментариев
+      factory :article_with_comments do
+        # после создания article
+      after :create do |article, evaluator|
+          # создаём список из 3-х комментариев
       create_list :comment, 3, article: article
       end
     end
