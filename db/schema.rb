d65f7d38cf652d50849c8d97db60b777b49b678a
AntoMonti <a.matviishyn@hotmail.com>
2020-02-24 18:57:14 +0200

    rails db:migrate after adding image:binary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_062349) do
+ActiveRecord::Schema.define(version: 2020_02_24_165250) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_062349) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "subtext"
+    t.binary "image"
   end
 
   create_table "comments", force: :cascade do |t|
